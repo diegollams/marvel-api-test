0ab47b96dc456f0f3144a21bd4bb17222fbfec27
Diego Llamas <diegollams@gmail.com>
2016-10-29 09:21:02 -0500

    Launches screen and organise files

---
+++ marvel-api-test/MarvelApiUtil.swift
@@ -0,0 +1,13 @@
+//
+//  MarvelApiUtil.swift
+//  marvel-api-test
+//
+//  Created by Diego Llamas Velasco on 10/29/16.
+//  Copyright © 2016 Diego Llamas Velasco. All rights reserved.
+//
+
+import Foundation
+
+class MarvelApiUtil{
+    
+}
